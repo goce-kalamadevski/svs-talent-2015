d4ec31b21c77609374d4f801878f57811d4dfe33
goce-kalamadevski <kalamadevski@gmail.com>
2015-04-24 14:46:49 +0200

    Registar aplication + BL

---
+++ 04-SPA/Project/Registar/Registar.BusinessLayer/Handlers/BikeSearchCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Registar.BusinessLayer.Contracts;
+
+namespace Registar.BusinessLayer.Handlers
+{
+    internal class BikeSearchCommandHandler:CommandHandlerBase<BikeSearchCommand,BikeSearchResult>
+    {
+        protected override BikeSearchResult ExecuteCommand(BikeSearchCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
